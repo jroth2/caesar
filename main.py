86e0cb059291ad951054b67aedd470be1fd3ccdf
joe <jroth213@gmail.com>
2026-05-11 12:06:00 -0500

    adds arguments for reading files, encrypting, and decrypting

--- main.py
+++ main.py
@@ -5,27 +5,34 @@ CHARSET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def main():
     args = parse_args()
-    print(encrypt_string(args.string,args.shift))
+    if args.file:
+        with open(args.string,"r") as f:
+            input_str = f.read()
+        if args.encrypt:
+            print(encrypt_string(input_str,args.shift))
+        elif args.decrypt:
+            print("not yet implemented")
+
+    elif args.encrypt:
+        print(encrypt_string(args.string,args.shift))
+    elif args.decrypt:
+        print("not yet implemented")
+    
 
 def parse_args(argv=None):
-    parser = argparse.ArgumentParser(
-        description="Encrypt one or more characters using a Caesar shift."
-    )
-    parser.add_argument(
-        "string",
-        help="A string to be encoded or decoded"
+    parser = argparse.ArgumentParser(description="Encrypt one or more characters using a Caesar shift.")
 
-    )
-    parser.add_argument(
-        "shift",
-        type=int,
-        default=1,
-        help="Cipher offset to apply from 1-25 (default: 1).",
-    )
+    parser.add_argument("string",help="A string to be encoded or decoded")
+    parser.add_argument("-f", "--file", action="store_true", help="When added, the string argument is treated as a file path to read the string from.")
+    parser.add_argument("-e", "--encrypt",action="store_true", help="Sets script to encrypt")
+    parser.add_argument("-d", "--decrypt",action="store_true", help="Sets script to decrypt")
+    parser.add_argument("-s", "--shift", type=int, default=1, help="Cipher offset to apply from 1-25 (default: 1).")
 
     args = parser.parse_args(argv)
     if args.shift > len(CHARSET):
         args.shift = len(CHARSET)
+
+    print(args)
 
     return args
 
